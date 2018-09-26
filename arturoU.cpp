6e05c6cf37542521c811fc61c2aebcac1e3c9cd6
Arturo Urbina <aurbina@odin.cs.csubak.edu>
2018-09-26 10:51:16 -0700

    added credit state functions

--- arturoU.cpp
+++ arturoU.cpp
@@ -1,9 +1,21 @@
+//Arturo Urbina
+//date:September 25, 2018
+//defined variables and functions to toggle Credits
 #include <iostream>
 #include "fonts.h"
+static int creditsState;
+int getCreditState()
+{
+	return creditsState;
+}
+void toggleCredits()
+{
+	creditsState^=1;
+}
 void  art_credits(int x, int y)
 {
 	Rect r;
 	r.bot =y;
 	r.left=x;
-	ggprint8b(&r,16,0X00ff0000,"Arturo Urbina");
+	ggprint8b(&r,16,0x00FFFF00,"Arturo Urbina");
 }
