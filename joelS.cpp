3e5b6912d14c9983dd9fea873dd36f2c829d1ee1
JBSTG <31240922+JBSTG@users.noreply.github.com>
2018-10-02 11:31:01 -0700

    Update joelS.cpp
    Added joel_picture function.

--- joelS.cpp
+++ joelS.cpp
@@ -2,6 +2,7 @@
 //Author: Joel Staggs
 
 #include <iostream>
+#include <GL/glx.h>
 #include "fonts.h"
 
 
@@ -12,4 +13,23 @@ void joel_credits(int x, int y){
         r.left = x;
 
         ggprint8b(&r, 16, 0x00ff0000, "Joel Staggs");
+}
+
+void joel_picture(int x, int y, GLuint texture){
+
+
+        glColor3ub(255,255,255);
+        int wid=100;
+        glPushMatrix();
+        glTranslatef(x,y,0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+        glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+        glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+        glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+
+        glEnd();
+        glPopMatrix();
+
 }
