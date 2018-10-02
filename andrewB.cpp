b007af93d00b5a4182ba2c07c381818a396981de
Andrew Burt <42781117+aburt1@users.noreply.github.com>
2018-10-02 11:38:58 -0700

    Add files via upload

--- andrewB.cpp
+++ andrewB.cpp
@@ -6,8 +6,8 @@ using namespace std;
 void andrew_credits(int x, int y)
 {
     Rect r;
-    r.bot = y;
-    r.left = x;
+    r.bot = x;
+    r.left = y;
     ggprint8b(&r, 16, 0x00ff0000, "Andrew Burt");
 }
 void andrew_credits_picture(int x, int y, GLuint texid)
