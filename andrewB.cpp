4025c1e41baf8da0b2e1e0b915908fd89e05363e
Andrew Burt <42781117+aburt1@users.noreply.github.com>
2018-10-08 21:17:37 -0700

    Added image to ship

--- andrewB.cpp
+++ andrewB.cpp
@@ -1,4 +1,5 @@
 //Resource file for Andrew Burt.
+//This file will include the functions to display graphics to the program.
 #include <iostream>
 #include "fonts.h"
 #include <GL/glx.h>
@@ -10,12 +11,28 @@ void andrew_credits(int x, int y)
     r.left = y;
     ggprint8b(&r, 16, 0x00ff0000, "Andrew Burt");
 }
-void andrew_credits_picture(int x, int y, GLuint texid)
+void andrew_picture(int x, int y, GLuint texid)
 {
     glColor3ub(255, 255, 255);
-    int w=40;
+    int w=50;
     glPushMatrix();
     glTranslatef(x, y, 0);
+    glBindTexture(GL_TEXTURE_2D, texid);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f); glVertex2f(-w,  -w);
+    glTexCoord2f(0.0f, 0.0f); glVertex2f( -w,  w);
+    glTexCoord2f(1.0f, 0.0f); glVertex2f( w, w);
+    glTexCoord2f(1.0f, 1.0f); glVertex2f( w, -w);
+    glEnd();
+    glPopMatrix();
+}
+void character(int x, int y, int z, float angle, GLuint texid)
+{
+    glPushMatrix();
+    glColor3ub(255, 255, 255);
+    int w = 50; 
+	glTranslatef(x, y, z);
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
     glBindTexture(GL_TEXTURE_2D, texid);
     glBegin(GL_QUADS);
     glTexCoord2f(0.0f, 1.0f); glVertex2f(-w,  -w);
