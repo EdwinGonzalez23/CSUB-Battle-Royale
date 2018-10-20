869f653dcde0a167082d63f53fa79ad69c36c239
edwin <gonzalez.edwin2311@gmail.com>
2018-10-20 12:02:41 -0700

    AI tracking added, Player shooting still wonky, Bullet time struct causing AI shooting synchronization

--- csub.cpp
+++ csub.cpp
@@ -40,7 +40,7 @@ const float TIMESLICE = 1.0f;
 const float GRAVITY = -0.6f;
 #define PI 3.141592653589793
 #define ALPHA 1
-const int MAX_BULLETS = 100;
+const int MAX_BULLETS = 1000;
 const Flt MINIMUM_ASTEROID_SIZE = 10.0;
 //-----------------------------------------------------------------------------
 //Setup timers
@@ -172,9 +172,12 @@ class Game {
 		Ship ship;
 		Asteroid *ahead;
 		Bullet *barr;
+		Bullet *barrAst;
 		int nasteroids;
 		int nbullets;
+		int astBull;
 		struct timespec bulletTimer;
+		struct timespec enemyBulletTimer;
 		struct timespec mouseThrustTimer;
 		bool mouseThrustOn;
 	public:
@@ -182,11 +185,13 @@ class Game {
 			astr_destroyed=0;
 			ahead = NULL;
 			barr = new Bullet[MAX_BULLETS];
+			barrAst = new Bullet[MAX_BULLETS];
 			nasteroids = 0;
 			nbullets = 0;
+			astBull = 0;
 			mouseThrustOn = false;
 			//build 10 asteroids...
-			for (int j=0; j<10; j++) {
+			for (int j=0; j<3; j++) { //Change num asteroid
 				Asteroid *a = new Asteroid;
 				a->nverts = 8;
 				a->radius = rnd()*80.0 + 40.0;
@@ -219,9 +224,11 @@ class Game {
 				++nasteroids;
 			}
 			clock_gettime(CLOCK_REALTIME, &bulletTimer);
+			clock_gettime(CLOCK_REALTIME, &enemyBulletTimer);
 		}
 		~Game() {
 			delete [] barr;
+			delete [] barrAst;
 		}
 } g;
 //X Windows variables
@@ -689,27 +696,38 @@ void physics()
 		//move the bullet
 		b->pos[0] += b->vel[0];
 		b->pos[1] += b->vel[1];
-		//Check for collision with window edges
-		if (b->pos[0] < 0.0) {
-			b->pos[0] += (float)gl.xres;
-		}
-		else if (b->pos[0] > (float)gl.xres) {
-			b->pos[0] -= (float)gl.xres;
-		}
-		else if (b->pos[1] < 0.0) {
-			b->pos[1] += (float)gl.yres;
-		}
-		else if (b->pos[1] > (float)gl.yres) {
-			b->pos[1] -= (float)gl.yres;
-		}
 		i++;
 	}
 	//
+	//update asteroid bullet position
+	int j = 0;
+	while (j < g.astBull) {
+		Bullet *bAst = &g.barrAst[j];
+		double ts = timeDiff(&bAst->time, &bt);
+		if (ts > 2.5) {
+			memcpy(&g.barrAst[j], &g.barrAst[g.astBull-1],
+					sizeof(Bullet));
+			g.astBull--;
+			continue;
+		}
+	 	bAst->pos[0] += bAst->vel[0];
+	 	bAst->pos[1] += bAst->vel[1];
+		j++;
+	}
+	//
 	//Update asteroid positions
+	extern float unitLock(float);
+	extern float lockOnAngle(float, float, float, float);
+	extern float setTrigger(float, float, float, float);
+	float angleLockOn, trigger;
+	float accuracy = 90;
+	float triggerDist = 200;
 	Asteroid *a = g.ahead;
+	a->angle = unitLock(a->angle);
 	while (a) {
 		a->pos[0] += a->vel[0];
 		a->pos[1] += a->vel[1];
+		a->angle += a->rotate;
 		//Check for collision with window edges
 		if (a->pos[0] < -100.0) {
 			a->pos[0] += (float)gl.xres+200;
@@ -723,8 +741,45 @@ void physics()
 		else if (a->pos[1] > (float)gl.yres+100) {
 			a->pos[1] -= (float)gl.yres+200;
 		}
-		a->angle += a->rotate;
-		a = a->next;
+		Bullet *bAst = &g.barrAst[g.astBull];
+		trigger = setTrigger(a->pos[0], a->pos[1], g.ship.pos[0], g.ship.pos[1]);
+		//
+		//All angles have been calculated, shoot if triggered
+		if (trigger < triggerDist) {
+			angleLockOn = lockOnAngle(g.ship.pos[0], a->pos[0], g.ship.pos[1], a->pos[1]);
+			a->angle = angleLockOn - accuracy;
+			//a little time between each bullet
+			double ts = timeDiff(&g.enemyBulletTimer, &bt);
+			if (ts > 0.1) {
+				timeCopy(&g.enemyBulletTimer, &bt);
+				if (g.astBull < MAX_BULLETS) {
+					//shoot a bullet...
+					timeCopy(&bAst->time, &bt);
+					bAst->pos[0] = a->pos[0];
+					bAst->pos[1] = a->pos[1];
+					bAst->vel[0] = (a->vel[0]);
+					bAst->vel[1] = (a->vel[1]);
+					//convert ship angle to radians
+					Flt rad = ((a->angle+90.0) / 360.0f) * PI * 2.0;
+					//convert angle to a vector
+					Flt xdir = cos(rad);
+					Flt ydir = sin(rad);
+					bAst->pos[0] += xdir*20.0f;
+					bAst->pos[1] += ydir*20.0f;
+					bAst->vel[0] += xdir*6.0f + rnd()*0.1;
+					bAst->vel[1] += ydir*6.0f + rnd()*0.1;
+					bAst->color[0] = 1.0f;
+					bAst->color[1] = 1.0f;
+					bAst->color[2] = 1.0f;
+					g.astBull++;
+					//Reload
+					if (g.astBull == MAX_BULLETS) {
+						g.astBull = 0;
+					}
+				}
+			}
+		}
+			a = a->next;
 	}
 	//
 	//Asteroid collision with bullets?
@@ -732,57 +787,57 @@ void physics()
 	//   1. delete the bullet
 	//   2. break the asteroid into pieces
 	//      if asteroid small, delete it
-	a = g.ahead;
-	while (a) {
-		//is there a bullet within its radius?
-		int i=0;
-		while (i < g.nbullets) {
-			Bullet *b = &g.barr[i];
-			d0 = b->pos[0] - a->pos[0];
-			d1 = b->pos[1] - a->pos[1];
-			dist = (d0*d0 + d1*d1);
-			if (dist < (a->radius*a->radius)) {
-				//cout << "asteroid hit." << endl;
-				//this asteroid is hit.
-				if (a->radius > MINIMUM_ASTEROID_SIZE) {
-				// 	//break it into pieces.
-				// 	Asteroid *ta = a;
-				// 	buildAsteroidFragment(ta, a);
-				// 	int r = rand()%10+5;
-				// 	for (int k=0; k<r; k++) {
-				// 		//get the next asteroid position in the array
-				// 		Asteroid *ta = new Asteroid;
-				// 		buildAsteroidFragment(ta, a);
-				// 		//add to front of asteroid linked list
-				// 		ta->next = g.ahead;
-				// 		if (g.ahead != NULL)
-				// 			g.ahead->prev = ta;
-				// 		g.ahead = ta;
-				// 		g.nasteroids++;
-				// 	}
-				// } else {
-					a->color[0] = 1.0;
-					a->color[1] = 0.1;
-					a->color[2] = 0.1;
-					//asteroid is too small to break up
-					//delete the asteroid and bullet
-					Asteroid *savea = a->next;
-					deleteAsteroid(&g, a);
-					a = savea;
-					g.nasteroids--;
-				}
-				//delete the bullet...
-				memcpy(&g.barr[i], &g.barr[g.nbullets-1], sizeof(Bullet));
-				g.nbullets--;
-				if (a == NULL)
-					break;
-			}
-			i++;
-		}
-		if (a == NULL)
-			break;
-		a = a->next;
-	}
+	// a = g.ahead;
+	// while (a) {
+	// 	//is there a bullet within its radius?
+	// 	int i=0;
+	// 	while (i < g.nbullets) {
+	// 		Bullet *b = &g.barr[i];
+	// 		d0 = b->pos[0] - a->pos[0];
+	// 		d1 = b->pos[1] - a->pos[1];
+	// 		dist = (d0*d0 + d1*d1);
+	// 		if (dist < (a->radius*a->radius)) {
+	// 			//cout << "asteroid hit." << endl;
+	// 			//this asteroid is hit.
+	// 			if (a->radius > MINIMUM_ASTEROID_SIZE) {
+	// 			// 	//break it into pieces.
+	// 			// 	Asteroid *ta = a;
+	// 			// 	buildAsteroidFragment(ta, a);
+	// 			// 	int r = rand()%10+5;
+	// 			// 	for (int k=0; k<r; k++) {
+	// 			// 		//get the next asteroid position in the array
+	// 			// 		Asteroid *ta = new Asteroid;
+	// 			// 		buildAsteroidFragment(ta, a);
+	// 			// 		//add to front of asteroid linked list
+	// 			// 		ta->next = g.ahead;
+	// 			// 		if (g.ahead != NULL)
+	// 			// 			g.ahead->prev = ta;
+	// 			// 		g.ahead = ta;
+	// 			// 		g.nasteroids++;
+	// 			// 	}
+	// 			// } else {
+	// 				a->color[0] = 1.0;
+	// 				a->color[1] = 0.1;
+	// 				a->color[2] = 0.1;
+	// 				//asteroid is too small to break up
+	// 				//delete the asteroid and bullet
+	// 				Asteroid *savea = a->next;
+	// 				deleteAsteroid(&g, a);
+	// 				a = savea;
+	// 				g.nasteroids--;
+	// 			}
+	// 			//delete the bullet...
+	// 			memcpy(&g.barr[i], &g.barr[g.nbullets-1], sizeof(Bullet));
+	// 			g.nbullets--;
+	// 			if (a == NULL)
+	// 				break;
+	// 		}
+	// 		i++;
+	// 	}
+	// 	if (a == NULL)
+	// 		break;
+	// 	a = a->next;
+	// }
 	//---------------------------------------------------
 	//check keys pressed now
 	extern float moveDownSpeed();
@@ -966,28 +1021,73 @@ extern int  getCreditState();
 		//DrawCircle((float)gl.xres/2,(float)gl.yres/2,44.0,33);
 		//------------------
 		//Draw the asteroids
-		{
-			Asteroid *a = g.ahead;
-			while (a) {
-				//Log("draw asteroid...\n");
-				glColor3fv(a->color);
-				glPushMatrix();
-				glTranslatef(a->pos[0], a->pos[1], a->pos[2]);
-				glRotatef(a->angle, 0.0f, 0.0f, 1.0f);
-				glBegin(GL_LINE_LOOP);
-				//Log("%i verts\n",a->nverts);
-				for (int j=0; j<a->nverts; j++) {
-					glVertex2f(a->vert[j][0], a->vert[j][1]);
-				}
-				glEnd();
-				glPopMatrix();
-				glColor3f(1.0f, 0.0f, 0.0f);
-				glBegin(GL_POINTS);
-				glVertex2f(a->pos[0], a->pos[1]);
-				glEnd();
-				a = a->next;
-			}
+		Asteroid *a = g.ahead;
+		while (a) {
+		glColor3fv(a->color);
+		glPushMatrix();
+		glTranslatef(a->pos[0], a->pos[1], a->pos[2]);
+		glRotatef(a->angle, 0.0f, 0.0f, 1.0f);
+		glBegin(GL_TRIANGLE_FAN);
+		glVertex2f(-12.0f, -10.0f);
+		glVertex2f(  0.0f, 20.0f);
+		glVertex2f(  0.0f, -6.0f);
+		glVertex2f(  0.0f, -6.0f);
+		glVertex2f(  0.0f, 20.0f);
+		glVertex2f( 12.0f, -10.0f);
+		glEnd();
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glBegin(GL_POINTS);
+		glVertex2f(0.0f, 0.0f);
+		glEnd();
+		glPopMatrix();
+        extern void character(int x, int y, int z, float angle, GLuint texid);
+        //character(g.ship.pos[0], g.ship.pos[1], g.ship.pos[2], g.ship.angle, gl.characterTexture);
+		// if (gl.keys[XK_Up] || g.mouseThrustOn) {
+		// 	int i;
+		// 	//draw thrust
+		// 	Flt rad = ((g.ship.angle+90.0) / 360.0f) * PI * 2.0;
+		// 	//convert angle to a vector
+		// 	Flt xdir = cos(rad);
+		// 	Flt ydir = sin(rad);
+		// 	Flt xs,ys,xe,ye,r;
+		// 	glBegin(GL_LINES);
+		// 	for (i=0; i<16; i++) {
+		// 		xs = -xdir * 11.0f + rnd() * 4.0 - 2.0;
+		// 		ys = -ydir * 11.0f + rnd() * 4.0 - 2.0;
+		// 		r = rnd()*40.0+40.0;
+		// 		xe = -xdir * r + rnd() * 18.0 - 9.0;
+		// 		ye = -ydir * r + rnd() * 18.0 - 9.0;
+		// 		glColor3f(rnd()*.3+.7, rnd()*.3+.7, 0);
+		// 		glVertex2f(g.ship.pos[0]+xs,g.ship.pos[1]+ys);
+		// 		glVertex2f(g.ship.pos[0]+xe,g.ship.pos[1]+ye);
+		// 	}
+			glEnd();
+			a = a->next;
 		}
+		//ASTEROID HAS BEEN REPLACE WITH SHIP FOR TESTING
+
+		// {
+		// 	Asteroid *a = g.ahead;
+		// 	while (a) {
+		// 		//Log("draw asteroid...\n");
+		// 		glColor3fv(a->color);
+		// 		glPushMatrix();
+		// 		glTranslatef(a->pos[0], a->pos[1], a->pos[2]);
+		// 		glRotatef(a->angle, 0.0f, 0.0f, 1.0f);
+		// 		glBegin(GL_LINE_LOOP);
+		// 		//Log("%i verts\n",a->nverts);
+		// 		for (int j=0; j<a->nverts; j++) {
+		// 			glVertex2f(a->vert[j][0], a->vert[j][1]);
+		// 		}
+		// 		glEnd();
+		// 		glPopMatrix();
+		// 		glColor3f(1.0f, 0.0f, 0.0f);
+		// 		glBegin(GL_POINTS);
+		// 		glVertex2f(a->pos[0], a->pos[1]);
+		// 		glEnd();
+		// 		a = a->next;
+		// 	}
+		// }
 		//----------------
 		//Draw the bullets
 		Bullet *b = &g.barr[0];
@@ -1007,6 +1107,25 @@ extern int  getCreditState();
 			glVertex2f(b->pos[0]+1.0f, b->pos[1]+1.0f);
 			glEnd();
 			++b;
+		}
+		//CHANGE draw ast bullet
+		Bullet *bAst = &g.barrAst[0];
+		for (int i=0; i<g.astBull; i++) {
+			//Log("draw bullet...\n");
+			glColor3f(1.0, 1.0, 1.0);
+			glBegin(GL_POINTS);
+			glVertex2f(bAst->pos[0],      bAst->pos[1]);
+			glVertex2f(bAst->pos[0]-1.0f, bAst->pos[1]);
+			glVertex2f(bAst->pos[0]+1.0f, bAst->pos[1]);
+			glVertex2f(bAst->pos[0],      bAst->pos[1]-1.0f);
+			glVertex2f(bAst->pos[0],      bAst->pos[1]+1.0f);
+			glColor3f(0.8, 0.8, 0.8);
+			glVertex2f(bAst->pos[0]-1.0f, bAst->pos[1]-1.0f);
+			glVertex2f(bAst->pos[0]-1.0f, bAst->pos[1]+1.0f);
+			glVertex2f(bAst->pos[0]+1.0f, bAst->pos[1]-1.0f);
+			glVertex2f(bAst->pos[0]+1.0f, bAst->pos[1]+1.0f);
+			glEnd();
+			++bAst;
 		}
 	}
 }
--- edwinG.cpp
+++ edwinG.cpp
@@ -7,6 +7,7 @@
 #include "fonts.h"
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <math.h>
 
 void edwin_credits(int x, int y){
 	Rect r;
@@ -32,6 +33,13 @@ void edwin_picture(int x, int y, GLuint texture)
     glPopMatrix();
 }
 
+int checkDistApart(int X1, int X2, int Y1, int Y2)
+{
+		//int X1, X2, Y1, Y2;
+		int X = abs(X2 - X1);
+		int Y = abs(Y2 - Y1);
+		return sqrt((pow(X, 2)) - (pow(Y, 2)));
+}
 
 float moveDownSpeed()
 {
@@ -52,3 +60,60 @@ float moveRightSpeed()
 {
 	return 7.0f;
 }
+
+float unitLock(float angle)
+{
+	if (angle >= 360.0f)
+		return angle - 360.0f;
+}
+
+float lockOnAngle(float xShipPos, float xAstPos, float yShipPos, float yAstPos)
+{
+	float x = (xShipPos - xAstPos);
+	float y = (yShipPos - yAstPos);
+	float divTan = (y/x);
+	float lockOnAngle;
+	if (x < 0 && y >= 0) {
+		lockOnAngle = (atan(divTan) * 180 / 3.1415) + 180;
+	}
+	else if (x < 0 && y < 0) {
+		lockOnAngle = (atan(divTan) * 180 / 3.1415) + 180;
+	}
+	else if (x >= 0 && y <= 0) {
+		lockOnAngle = (atan(divTan) * 180 / 3.1415) + 360;
+	} else {
+		lockOnAngle = abs(atan(divTan) * 180 / 3.1415);
+	}
+	return lockOnAngle;
+}
+
+float setTrigger(float xAstPos, float yAstPos, float xShipPos, float yShipPos)
+{
+	float astXY, shipXY, trigger;
+ 	astXY = xAstPos + yAstPos;
+	shipXY = xShipPos + yShipPos;
+	return trigger = abs(astXY - shipXY);
+}
+
+
+
+
+//Testing Console Log (ignore)
+// cout << "\n";
+// 		cout << "Asteroid PosX: " << a->pos[0] << endl;
+// 		cout << "Asteroid PosY: " << a->pos[1] << endl;
+// 		cout << "SHIP X: " << g.ship.pos[0] << endl;
+// 		cout << "SHIP Y: "<< g.ship.pos[1] << endl;
+// 		cout << "xTan: " << xTan << endl;
+// 		cout << "yTan: "<< yTan << endl;
+// 		//cout << "divTan: "<< divTan << endl;
+// 		//cout << "divTanLow: "<< divTanLow << endl;
+// 		cout << "\ndivTanAngle: "<< divTanAngle << endl;
+// 		cout << "\nAsteroid rot: " << a->rotate << endl;
+// 		cout << "Asteroid angle: " << a->angle << endl;
+// 		cout << "Ship angle: " << g.ship.angle << endl;
+// 		Flt radss = ((a->angle+90.0) / 360.0f) * PI * 2.0;
+// 		cout << "Rad Ast Angle: " << radss << endl;
+//
+//
+// 		cout << "\n";
