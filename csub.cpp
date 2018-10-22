4a9c7f0502dbcef88b35e60ea531f092f601de29
Student <student@lab-walkin-013.cs.csubak.edu>
2018-10-22 14:20:31 -0700

    Merged Edwin with Master. Movement and weapons now work together

--- csub.cpp
+++ csub.cpp
@@ -1006,17 +1006,7 @@ void physics()
 			g.mouseThrustOn = false;
 	}
 }
-void DrawCircle(float cx, float cy, float r, int num_segments)
-{
-	glBegin(GL_LINE_LOOP);
-	for(int ii = 0; ii < num_segments; ii++)
-	{
-		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
-		float x = r * cosf(theta);//calculate the x component
-		float y = r * sinf(theta);//calculate the y component
-		glVertex2f(x + cx, y + cy);//output vertex
-	}
-	glEnd();
+
 void firePistol(){
 	struct timespec bt;
 	clock_gettime(CLOCK_REALTIME, &bt);
